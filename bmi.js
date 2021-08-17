a7565138a5e18182730181cd1f4412c3d04d9539
Guilherme Ferreira Camapum <guilherme.camapum@ab-inbev.com>
2021-08-16 21:32:05 -0300

    Refactor functional programming

---
+++ bmi.js
@@ -0,0 +1,25 @@
+const bmiValues = [
+    {
+        min: 0,
+        max: 18.5,
+        imc: 'Magreza'
+    },
+    {
+        min: 18.5,
+        max: 24.9,
+        imc: 'Normal'
+    },
+    {
+        min: 24.9,
+        max: 30,
+        imc: 'Sobrepeso'
+    },
+    {
+        min: 30,
+        max: 36,
+        imc: 'Obesidade'
+    }
+]
+
+
+module.exports = bmiValues
--- example.js
+++ example.js
@@ -1,45 +1,11 @@
-// altura 1.76 peso 79
-// resposta: magreza, sobrepeso, normal, obeso
+const bmi = require('./bmi')
 
-//Magreza, quando o resultado é menor que 18.5 kg / m2;
-//Normal, quando o resultado está entre 18.5 e 24.9 kg / m2;
-//Sobrepeso, quando o resultado está entre 24.9 e 30 kg / m2;
-//Obesidade, quando o resultado é maior que 30 kg / m2.
-
-const valoresIMC = [
-    {
-        min: 0,
-        max: 18.5,
-        imc: 'Magreza'
-    },
-    {
-        min: 18.5,
-        max: 24.9,
-        imc: 'Normal'
-    },
-    {
-        min: 24.9,
-        max: 30,
-        imc: 'Sobrepeso'
-    },
-    {
-        min: 30,
-        max: 36,
-        imc: 'Obesidade'
+const personData = {
+    height: 1.76,
+    weight: 79,
+    average: function () {
+        return this.weight / (this.height ^ 2)
     }
-]
-
-var altura = 1.76,
-    peso = 79,
-    media = peso / (altura * altura)
-
-function obtemIMC() {
-    valoresIMC.forEach(function (valorImc) {
-        if (media.between(valorImc.min, valorImc.max))
-            meuIMC = valorImc
-    })
-
-    return meuIMC
 }
 
 Number.prototype.between = function (minimo, maximo) {
@@ -49,4 +15,12 @@ Number.prototype.between = function (minimo, maximo) {
     return this >= min && this <= max
 }
 
-console.log(obtemIMC())
+const personBmi = (average, bmiArray) => {
+    for (const existentBmi of bmiArray) {
+        if (average.between(existentBmi.min, existentBmi.max)) {
+            return existentBmi
+        }
+    }
+}
+
+console.log(personBmi(personData.average(), bmi))
